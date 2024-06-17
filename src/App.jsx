2a976fa86a087ef20a0074a4ff361171475a486b
Sumin893 <ea0307@naver.com>
2024-06-18 02:18:13 +0900

    styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css'
+// import React from 'react'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #ffe4c4;
+  width: 100%;
+  height: 50vh;
+`;
+const StyledLine = styled.div`
+  font-size: 30px;
+  color: #f08080;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
 
   return (
     <>
-      여기가 App 페이지입니다!
+      <Wrapper>
+        <StyledLine>Hello World!</StyledLine>
+      </Wrapper>
     </>
   )
 }
