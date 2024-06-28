24e56d0754d2b5b3c52337f5524a04fd164074fc
Sumin893 <ea0307@naver.com>
2024-06-29 03:57:52 +0900

    이벤트 2. onChange

--- CarrotMarket/market-project/src/pages/Event/Event.jsx
+++ CarrotMarket/market-project/src/pages/Event/Event.jsx
@@ -1,11 +1,21 @@
 const button = () => {
   return (
+    <>
     <button onClick={() => alert("버튼 클릭했네요?")}>Button</button>
+    <input 
+      type="text"
+      onChange={(e)=>{console.log(e.target.value)}}
+    >
+    </input>
+    </>
   )
 }
 
 //    <button onClick={alert("버튼 클릭했네요?")}>Button</button>
 // 위에 처럼 하면 버튼을 클릭하지 않았는데도 alert 저게 뜸. ==>>>화살표 함수를 사용해야함!!!!
+
+//button은 onClick, input은 onChange
+//onChange: input의 텍스트가 바뀔 때마다 발생하는 이벤트, 이벤트가 발생하면 handleChange 함수가 작동하고, 이벤트 객체에 담긴 input값을 setState를 통해 새로운 state로 갱신.
 
 
 export default button
